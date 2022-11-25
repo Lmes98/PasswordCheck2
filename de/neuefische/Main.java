eed52abde0feb0aa02c326423e1c6a2027737d94
Lmes98 <118824069+Lmes98@users.noreply.github.com>
2022-11-25 12:55:30 +0100

    Test

--- de/neuefische/Main.java
+++ de/neuefische/Main.java
@@ -2,6 +2,61 @@ package de.neuefische;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
+
+    public static boolean testPasswordLength(String password){
+
+        int length = password.length();
+        if(length >= 8){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public static boolean testPasswordNumber(String password){
+
+        return password.matches(".*[0-9]");
+    }
+
+    public static boolean testPasswordUpperLower(String password){
+
+        int anz = 0;
+        for(char c : password.toCharArray()){
+            if(c >= 'A' && c <= 'Z'){
+                anz = anz + 1;
+            }
+
+        }
+
+        if(anz > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean testPasswordIfBad(String password) {
+    String[] arrayPassword = new String[5];
+
+    arrayPassword[0] = "passwort";
+    arrayPassword[1] ="123456";
+
+    boolean valid = true;
+
+    for(int i = 0; i < arrayPassword.length;i++){
+        if(password.equals(arrayPassword[i])){
+            valid = false;
+        }else{
+            valid = true;
+        }
+    }
+    return  valid;
+    }
+
+
+
 }
